8e56718f18bb0f1508e42b74809ec78c27a9abc7
Michael J. Forster <mike@forsterfamily.ca>
2018-03-07 09:56:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# cl-postgres-plus-uuid
+
+cl-postgres-plus-uuid is a Common Lisp library that integrates the
+UUID library with cl-postgres.
+
+cl-postgres-plus-uuid depends on
+[cl-postgres](http://marijnhaverbeke.nl/postmodern/cl-postgres.html)
+and [UUID](https://github.com/dardoria/uuid).
+
+cl-postgres-plus-uuid is being developed with
+[SBCL](http://sbcl.org/), [CCL](http://ccl.clozure.com/), and
+[LispWorks](http://www.lispworks.com/) on OS X.  cl-postgres-plus-uuid
+is being deployed with SBCL on FreeBSD/AMD64 and Linux/AMD64.
+
+
+### Installation
+
+```lisp
+(ql:quickload "cl-postgres-plus-uuid")
+```
+
+### Example
+
+```lisp
+(defvar *connection* (cl-postgres:open-database *dbname* *dbuser* *dbpassword* *dbhost*))
+
+(cl-postgres:exec-query *connection* "SELECT uuid_generate_v4();" 'cl-postgres:list-row-reader)
+=> (("ce29603b-59b8-4354-9e3c-3798a10451de"))
+
+(cl-postgres-plus-uuid:set-uuid-sql-reader)
+
+(cl-postgres:exec-query *connection* "SELECT uuid_generate_v4();" 'cl-postgres:list-row-reader)
+=> ((C526D222-9A51-4453-99D1-FCC691826020))
+
+(type-of (caar *))
+=> UUID:UUID
+
+(cl-postgres:close-database *connection*)
+```
+
+### License
+
+cl-postgres-plus-uuid is distributed under the MIT license. See LICENSE.
---
+++ cl-postgres-plus-uuid.lisp
@@ -0,0 +1,43 @@
+;;;; cl-postgres-plus-uuid.lisp
+
+;;; The MIT License (MIT)
+;;;
+;;; Copyright (c) 2013 Michael J. Forster
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included in all
+;;; copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;;; SOFTWARE.
+
+(in-package "CL-POSTGRES-PLUS-UUID")
+
+(defconstant +uuid-oid+ 2950
+  "The value of the PostgreSQL Object Identifier (OID) for the UUID data type.
+
+Obtained from the PostgreSQL pg_type system catalogue via the
+following query:
+
+    SELECT oid FROM pg_type WHERE typname = 'uuid';
+
+See https://www.postgresql.org/docs/10/static/datatype-oid.html,
+https://www.postgresql.org/docs/10/static/datatype-uuid.html, and
+https://www.postgresql.org/docs/current/static/catalog-pg-type.html.")
+
+(defun set-uuid-sql-reader (&optional (table cl-postgres:*sql-readtable*))
+  (cl-postgres:set-sql-reader +uuid-oid+ #'uuid:make-uuid-from-string :table table))
+
+(defmethod cl-postgres:to-sql-string ((arg uuid:uuid))
+  (format nil "'~S'::uuid" arg))
