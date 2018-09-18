13818cff39f793c147da30c67fdca49e9c9425ec
Michael J. Forster <mike@forsterfamily.ca>
2018-09-18 14:39:22 -0500

    Fix method on CL-POSTGRES:TO-SQL-STRING

--- cl-postgres-plus-uuid.lisp
+++ cl-postgres-plus-uuid.lisp
@@ -40,4 +40,4 @@ https://www.postgresql.org/docs/current/static/catalog-pg-type.html.")
   (cl-postgres:set-sql-reader +uuid-oid+ #'uuid:make-uuid-from-string :table table))
 
 (defmethod cl-postgres:to-sql-string ((arg uuid:uuid))
-  (format nil "'~S'::uuid" arg))
+  (format nil "~(~A~)" arg))
